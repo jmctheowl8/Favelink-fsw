052ab13d077705c2e4a32b68a3aaf92ce3be18e1
jmctheowl8 <john.cabrera@lc.cuny.edu>
2023-12-10 21:53:32 -0500

    bulit server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+const express = require('express')
+const path = require('path')
+
+
+const app = express();
+
+const PORT = 3000
+
+const clientPath = Path.resolve(__dirname, '../client/dist')
+
+app.use(express.static(clientPath))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
