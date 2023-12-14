409dde4da9f7272511416660c3e00fa36fd4e6ae
jmctheowl8 <john.cabrera@lc.cuny.edu>
2023-12-13 21:01:37 -0500

    got ride of cor moduel since we created our own api routes and working on updateing the client useing api routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
-import { express } from 'express'
 const express = require('express')
-const cors = require('cors')
 const db = require('./db')
 const app = express()
 const port = 3000
 app.use(express.json())
-app.use(cors())
 app.get('/api/links', db.getLinks)
 app.listen(port, () => {
 console.log(`App running on port ${port}.`)
