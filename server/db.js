65d9848d0c1c73b5aca841a2a5b90e22767b921f
jmctheowl8 <john.cabrera@lc.cuny.edu>
2023-12-14 19:41:18 -0500

    changed the api routes to include paths and added async/await

--- server/db.js
+++ server/db.js
@@ -7,52 +7,69 @@ const pool = new Pool({
     port: 5432,
 })
 
-const getLinks = (request, res) => {
-    pool.query('SELECT url FROM favlinks ORDER BY id ASC', (error, result) => {
-    if (error) {
-    throw error
+const express = require('express');
+const router = express.Router();
+
+// GET /api/favlinks
+router.get('/favlinks', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM favlinks');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error fetching favlinks:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+  
+router.post('/favlinks', async (req, res) => {
+    try {
+      const { name, url } = req.body;
+  
+      if (!name || !url) {
+        return res.status(400).json({ error: 'Name and URL are required' });
+      }
+  
+      const result = await pool.query(
+        'INSERT INTO favlinks (name, url) VALUES ($1, $2) RETURNING *',
+        [name, url]
+      );
+  
+      res.status(201).json(result.rows[0]);
+    } catch (error) {
+      console.error('Error creating favlink:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-    res.status(200).json(result.rows)
-    })
-}
-
-const createUser = (request, response) => {
-    const { name, url } = request.body
+  });
   
-    pool.query('INSERT INTO favlinks (name, url) VALUES ($1, $2) RETURNING *', [name, url], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).send(`User added with ID: ${results.rows[0].id}`)
-    })
-  }
 
-  const updateUser = (request, response) => {
-    const id = parseInt(request.params.id)
-    const { name, url } = request.body
+  router.put('/favlinks/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, url } = req.body;
   
-    pool.query(
-      'UPDATE favlinks SET name = $1, url = $2 WHERE id = $3',
-      [name, url, id],
-      (error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).send(`User modified with ID: ${id}`)
-      }
-    )
-  }
-
-  const deleteUser = (request, response) => {
-    const id = parseInt(request.params.id)
+      const result = await pool.query(
+        'UPDATE favlinks SET name = $1, url = $2 WHERE id = $3 RETURNING *',
+        [name, url, id]
+      );
   
-    pool.query('DELETE FROM favlinks WHERE id = $1', [id], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(`User deleted with ID: ${id}`)
-    })
-  }
-module.exports = {
-    getLinks,deleteUser,createUser,updateUser
-}
+      res.json(result.rows[0]);
+    } catch (error) {
+      console.error('Error updating favlink:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+  router.delete('/favlinks/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      await pool.query('DELETE FROM favlinks WHERE id = $1', [id]);
+  
+      res.json({ message: 'Favlink deleted successfully' });
+    } catch (error) {
+      console.error('Error deleting favlink:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+  module.exports = router;
